1a513561fea4162d54ec9dae14768973f5a10608
dnnys <dnnys@users.noreply.github.com>
2021-03-23 21:03:43 +0000

    deploy: 9116aef1eba02083cd37e3d466665dc565aafd9c

--- js/photos.js
+++ js/photos.js
@@ -17,11 +17,12 @@ lazyload();
   <a href="/2016-abandoned-sovjet-stuff-vii" data-target="2016-abandoned-sovjet-stuff-vii" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>NIKON D5300</li>
-    <li>1/100</li>
-    <li><span class="aperture"><em>f</em>/</span>5.6</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/100</li>
+    <li><span class="aperture"><em>f</em>/</span>5.6</li> -->
+  </ul>
 </li>
 <li class="item " id="2016-abandoned-sovjet-stuff-vii" style="background-image: url('/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20VII-65ba1d.jpg')" title="2016 Abandoned Sovjet Stuff VII">
   <img class="lazyload" data-src="/photos/thumbnail/2016%20Abandoned%20Sovjet%20Stuff%20VII-ccf4df.jpg" src="/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20VII-65ba1d.jpg" height="2048" width="1366" />
@@ -36,11 +37,12 @@ lazyload();
   <a href="/2016-abandoned-sovjet-stuff-vi" data-target="2016-abandoned-sovjet-stuff-vi" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>NIKON D5300</li>
-    <li>1/100</li>
-    <li><span class="aperture"><em>f</em>/</span>5.6</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/100</li>
+    <li><span class="aperture"><em>f</em>/</span>5.6</li> -->
+  </ul>
 </li>
 <li class="item " id="2016-abandoned-sovjet-stuff-vi" style="background-image: url('/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20VI-65ba1d.jpg')" title="2016 Abandoned Sovjet Stuff VI">
   <img class="lazyload" data-src="/photos/thumbnail/2016%20Abandoned%20Sovjet%20Stuff%20VI-ccf4df.jpg" src="/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20VI-65ba1d.jpg" height="2048" width="1365" />
@@ -55,11 +57,12 @@ lazyload();
   <a href="/2016-abandoned-sovjet-stuff-v" data-target="2016-abandoned-sovjet-stuff-v" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>NIKON D5300</li>
-    <li>1/100</li>
-    <li><span class="aperture"><em>f</em>/</span>9.0</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/100</li>
+    <li><span class="aperture"><em>f</em>/</span>9.0</li> -->
+  </ul>
 </li>
 <li class="item " id="2016-abandoned-sovjet-stuff-v" style="background-image: url('/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20V-65ba1d.jpg')" title="2016 Abandoned Sovjet Stuff V">
   <img class="lazyload" data-src="/photos/thumbnail/2016%20Abandoned%20Sovjet%20Stuff%20V-ccf4df.jpg" src="/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20V-65ba1d.jpg" height="2048" width="1365" />
@@ -74,11 +77,12 @@ lazyload();
   <a href="/2016-abandoned-sovjet-stuff-iv" data-target="2016-abandoned-sovjet-stuff-iv" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>NIKON D5300</li>
-    <li>1/100</li>
-    <li><span class="aperture"><em>f</em>/</span>8.0</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/100</li>
+    <li><span class="aperture"><em>f</em>/</span>8.0</li> -->
+  </ul>
 </li>
 <li class="item " id="2016-abandoned-sovjet-stuff-iv" style="background-image: url('/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20IV-65ba1d.jpg')" title="2016 Abandoned Sovjet Stuff IV">
   <img class="lazyload" data-src="/photos/thumbnail/2016%20Abandoned%20Sovjet%20Stuff%20IV-ccf4df.jpg" src="/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20IV-65ba1d.jpg" height="2048" width="1366" />
@@ -93,11 +97,12 @@ lazyload();
   <a href="/2016-abandoned-sovjet-stuff-iii" data-target="2016-abandoned-sovjet-stuff-iii" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>NIKON D5300</li>
-    <li>1/100</li>
-    <li><span class="aperture"><em>f</em>/</span>3.5</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/100</li>
+    <li><span class="aperture"><em>f</em>/</span>3.5</li> -->
+  </ul>
 </li>
 <li class="item " id="2016-abandoned-sovjet-stuff-iii" style="background-image: url('/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20III-65ba1d.jpg')" title="2016 Abandoned Sovjet Stuff III">
   <img class="lazyload" data-src="/photos/thumbnail/2016%20Abandoned%20Sovjet%20Stuff%20III-ccf4df.jpg" src="/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20III-65ba1d.jpg" height="2048" width="1365" />
@@ -112,11 +117,12 @@ lazyload();
   <a href="/2016-abandoned-sovjet-stuff-i" data-target="2016-abandoned-sovjet-stuff-i" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>NIKON D5300</li>
-    <li>1/100</li>
-    <li><span class="aperture"><em>f</em>/</span>3.5</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/100</li>
+    <li><span class="aperture"><em>f</em>/</span>3.5</li> -->
+  </ul>
 </li>
 <li class="item " id="2016-abandoned-sovjet-stuff-i" style="background-image: url('/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20I-65ba1d.jpg')" title="2016 Abandoned Sovjet Stuff I">
   <img class="lazyload" data-src="/photos/thumbnail/2016%20Abandoned%20Sovjet%20Stuff%20I-ccf4df.jpg" src="/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20I-65ba1d.jpg" height="1365" width="2048" />
@@ -131,11 +137,12 @@ lazyload();
   <a href="/2016-exploring-annecy-ii" data-target="2016-exploring-annecy-ii" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>NIKON D5300</li>
-    <li>1/100</li>
-    <li><span class="aperture"><em>f</em>/</span>4.5</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/100</li>
+    <li><span class="aperture"><em>f</em>/</span>4.5</li> -->
+  </ul>
 </li>
 <li class="item " id="2016-exploring-annecy-ii" style="background-image: url('/photos/tint/2016%20Exploring%20Annecy%20II-65ba1d.jpg')" title="2016 Exploring Annecy II">
   <img class="lazyload" data-src="/photos/thumbnail/2016%20Exploring%20Annecy%20II-ccf4df.jpg" src="/photos/tint/2016%20Exploring%20Annecy%20II-65ba1d.jpg" height="2048" width="1365" />
@@ -150,11 +157,12 @@ lazyload();
   <a href="/2016-epic-flare" data-target="2016-epic-flare" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>Canon EOS 6D</li>
-    <li>1/200</li>
-    <li><span class="aperture"><em>f</em>/</span>8.0</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/200</li>
+    <li><span class="aperture"><em>f</em>/</span>8.0</li> -->
+  </ul>
 </li>
 <li class="item " id="2016-epic-flare" style="background-image: url('/photos/tint/2016%20Epic%20Flare-65ba1d.jpg')" title="2016 Epic Flare">
   <img class="lazyload" data-src="/photos/thumbnail/2016%20Epic%20Flare-ccf4df.jpg" src="/photos/tint/2016%20Epic%20Flare-65ba1d.jpg" height="1365" width="2048" />
@@ -169,11 +177,12 @@ lazyload();
   <a href="/2016-annecy-light" data-target="2016-annecy-light" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>Canon EOS 6D</li>
-    <li>1/800</li>
-    <li><span class="aperture"><em>f</em>/</span>8.0</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/800</li>
+    <li><span class="aperture"><em>f</em>/</span>8.0</li> -->
+  </ul>
 </li>
 <li class="item " id="2016-annecy-light" style="background-image: url('/photos/tint/2016%20Annecy%20Light-65ba1d.jpg')" title="2016 Annecy Light">
   <img class="lazyload" data-src="/photos/thumbnail/2016%20Annecy%20Light-ccf4df.jpg" src="/photos/tint/2016%20Annecy%20Light-65ba1d.jpg" height="2048" width="1365" />
@@ -188,11 +197,12 @@ lazyload();
   <a href="/2016-annecy-and-mountains" data-target="2016-annecy-and-mountains" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>Canon EOS 6D</li>
-    <li>1/640</li>
-    <li><span class="aperture"><em>f</em>/</span>8.0</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/640</li>
+    <li><span class="aperture"><em>f</em>/</span>8.0</li> -->
+  </ul>
 </li>
 <li class="item " id="2016-annecy-and-mountains" style="background-image: url('/photos/tint/2016%20Annecy%20and%20Mountains-65ba1d.jpg')" title="2016 Annecy and Mountains">
   <img class="lazyload" data-src="/photos/thumbnail/2016%20Annecy%20and%20Mountains-ccf4df.jpg" src="/photos/tint/2016%20Annecy%20and%20Mountains-65ba1d.jpg" height="2048" width="1365" />
@@ -207,11 +217,12 @@ lazyload();
   <a href="/2016-exploring-annecy" data-target="2016-exploring-annecy" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>Canon EOS 6D</li>
-    <li>1/1000</li>
-    <li><span class="aperture"><em>f</em>/</span>8.0</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/1000</li>
+    <li><span class="aperture"><em>f</em>/</span>8.0</li> -->
+  </ul>
 </li>
 <li class="item " id="2016-exploring-annecy" style="background-image: url('/photos/tint/2016%20Exploring%20Annecy-65ba1d.jpg')" title="2016 Exploring Annecy">
   <img class="lazyload" data-src="/photos/thumbnail/2016%20Exploring%20Annecy-ccf4df.jpg" src="/photos/tint/2016%20Exploring%20Annecy-65ba1d.jpg" height="2048" width="1365" />
@@ -226,11 +237,12 @@ lazyload();
   <a href="/2016-abandoned-sovjet-stuff-ii" data-target="2016-abandoned-sovjet-stuff-ii" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>Canon EOS 6D</li>
-    <li>1/400</li>
-    <li><span class="aperture"><em>f</em>/</span>8.0</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/400</li>
+    <li><span class="aperture"><em>f</em>/</span>8.0</li> -->
+  </ul>
 </li>
 <li class="item " id="2016-abandoned-sovjet-stuff-ii" style="background-image: url('/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20II-65ba1d.jpg')" title="2016 Abandoned Sovjet Stuff II">
   <img class="lazyload" data-src="/photos/thumbnail/2016%20Abandoned%20Sovjet%20Stuff%20II-ccf4df.jpg" src="/photos/tint/2016%20Abandoned%20Sovjet%20Stuff%20II-65ba1d.jpg" height="1365" width="2048" />
@@ -242,10 +254,11 @@ lazyload();
   <a href="/2016-exploring-annecy" data-target="2016-exploring-annecy" class="previous" title="Go to previous photo">
     <span>Previous</span>
   </a>
-  <!-- <ul class="meta">
+  <ul class="meta">
     <li>NIKON D5300</li>
-    <li>1/80</li>
-    <li><span class="aperture"><em>f</em>/</span>4.5</li>
-	</ul> -->
+    <li>2021</li>
+    <!-- <li>1/80</li>
+    <li><span class="aperture"><em>f</em>/</span>4.5</li> -->
+  </ul>
 </li>
 `);
