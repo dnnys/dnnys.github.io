888f5cadeb9cb19a502d0bef2682ee7cac36d5a7
Dennys <dnnys@users.noreply.github.com>
2021-06-08 22:47:38 +0200

    Next try

--- _plugins/photo_filter.rb
+++ _plugins/photo_filter.rb
@@ -1,11 +1,11 @@
 require "exifr"
+# EXIFR::JPEG.new(photo.path).date_time.to_s
 
 module Jekyll
   module PhotoFilter
     def photo_filter(files)
       photos = files.select {|photo| photo.relative_path.include?("original") }
       photos.sort_by do |photo|
-            # EXIFR::JPEG.new(photo.path).date_time.to_s
             File.new(photo.path).mtime.to_s
       end.reverse
     end
