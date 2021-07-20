e0b6cc8d569dc0ed5905f018d7be5f6c46418876
dnnys <dnnys@users.noreply.github.com>
2021-07-20 18:45:54 +0000

    deploy: 547c89d959fc40faa8bc03b66f47720bd1a8eaad

--- js/photos.js
+++ js/photos.js
@@ -7,6 +7,24 @@ container.innerHTML = html;
 openPhoto(id, url);
 lazyload();
 })(`
+<li class="item " id="aperitivo" style="background-image: url('/photos/tint/Aperitivo-65ba1d.jpg')" title="Aperitivo">
+  <img class="lazyload thumb" data-src="/photos/thumbnail/Aperitivo-ccf4df.jpg" src="/photos/tint/Aperitivo-65ba1d.jpg" height="1365" width="2048" />
+  <img class="lazyload fullres" data-src="/photos/large/Aperitivo-686656.jpg" src="/photos/tint/Aperitivo-65ba1d.jpg" height="1365" width="2048" />
+  <span class="full">
+    <span style="background-image: url('/photos/large/Aperitivo-686656.jpg')"></span>
+  </span>
+  <a class="open" href="/aperitivo" data-target="aperitivo">Open</a>
+  <a class="close" href="/">Close</a>
+  <a href="/backyard-views" data-target="backyard-views" class="next" title="Go to next photo">
+    <span>Next</span>
+  </a>
+  <ul class="meta">
+    <li>FUJIFILM</li>
+    <li>2018</li>
+    <!-- <li>1/1000</li> -->
+    <!-- <li><span class="aperture"><em>f</em>/</span>5.6</li> -->
+  </ul>
+</li>
 <li class="item " id="backyard-views" style="background-image: url('/photos/tint/Backyard%20Views-65ba1d.jpg')" title="Backyard Views">
   <img class="lazyload thumb" data-src="/photos/thumbnail/Backyard%20Views-ccf4df.jpg" src="/photos/tint/Backyard%20Views-65ba1d.jpg" height="2048" width="1366" />
   <img class="lazyload fullres" data-src="/photos/large/Backyard%20Views-686656.jpg" src="/photos/tint/Backyard%20Views-65ba1d.jpg" height="2048" width="1366" />
@@ -15,6 +33,9 @@ lazyload();
   </span>
   <a class="open" href="/backyard-views" data-target="backyard-views">Open</a>
   <a class="close" href="/">Close</a>
+  <a href="/aperitivo" data-target="aperitivo" class="previous" title="Go to previous photo">
+    <span>Previous</span>
+  </a>
   <a href="/palazzi-views" data-target="palazzi-views" class="next" title="Go to next photo">
     <span>Next</span>
   </a>
